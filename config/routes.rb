6f34c5ffa1d6ba973f8706e51c5b430a0e885353
Ptitbab <brian.variengien@gmail.com>
2022-06-15 17:55:18 +0200

    user profile and upload avatar

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  resources :users do
+    resources :avatars
+  end
+
   devise_for :users
 root 'static_pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
