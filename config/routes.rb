262df60c1ec3baa2ee144b8b352c1742e54d9d0f
Eva Laurent <eva@macbook-pro-de-eva.home>
2022-06-16 15:36:25 +0200

    routes d-for dogs

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,11 @@
 Rails.application.routes.draw do
-  get 'dogs/index'
-  get 'dogs/show'
-  get 'dogs/create'
-  get 'dogs/update'
-  get 'dogs/destroy'
+
   devise_for :users
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   resources :users do
     resources :avatars
   end
+  resources :dogs
 
 
 root 'static_pages#home'
