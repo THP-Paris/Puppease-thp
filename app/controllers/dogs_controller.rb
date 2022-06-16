a0f69cc3d496712f41338ac177afe1f57e8765ab
Eva Laurent <eva@macbook-pro-de-eva.home>
2022-06-16 15:42:05 +0200

    view pages dog

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,11 +1,23 @@
 class DogsController < ApplicationController
   def index
+    @dog = Dog.all
   end
 
   def show
+    @dog = Dog.find(params[:id])
   end
 
+  def new
+    @dog = Dog.new
+  end 
+
   def create
+    @dog = Dog.new(dog_params)
+    if Dog.save 
+      redirect_to root_path
+    else
+      render 'new'
+    end
   end
 
   def update
@@ -13,4 +25,11 @@ class DogsController < ApplicationController
 
   def destroy
   end
+
+  private
+
+  def dog_params
+    params.require(:dog).permit(:name, :birth_date, :gender, :description, :vaccine, :lof, :reward, :breed, :color)
+  end
+
 end
--- app/models/dog.rb
+++ app/models/dog.rb
@@ -4,4 +4,7 @@ class Dog < ApplicationRecord
     enum gender: {male: 0, female: 1}
     enum breed: {Pitbull: 0, labrador: 1, golden:2, chihuahua: 3}
     enum color: {black: 0, white: 1, brown:2, sand:3}
+    enum vaccine: {vaccinated: 0, not_vaccinated: 1}
+    enum lof: {yes: 0, no: 1}
+    enum reward: {rewarded:0, not_rewarded:1}
 end
