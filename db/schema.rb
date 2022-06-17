6fac089724c8f97fdb3abb17a6d1e837ebea145a
Eva Laurent <eva@macbook-pro-de-eva.home>
2022-06-17 19:33:00 +0200

    index dogs + seed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_16_124904) do
+ActiveRecord::Schema.define(version: 2022_06_16_161908) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -87,7 +87,6 @@ ActiveRecord::Schema.define(version: 2022_06_16_124904) do
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.string "slug"
-    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["slug"], name: "index_users_on_slug", unique: true
