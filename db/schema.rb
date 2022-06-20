4804b0edbcb5b156028b04c06f32dbabf249e10c
Eva Laurent <eva@macbook-pro-de-eva.home>
2022-06-20 17:38:32 +0200

    page show dog ok

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_16_161908) do
+ActiveRecord::Schema.define(version: 2022_06_17_140714) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -87,10 +87,7 @@ ActiveRecord::Schema.define(version: 2022_06_16_161908) do
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.string "slug"
-<<<<<<< HEAD
     t.bigint "place_id"
-=======
->>>>>>> main
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["place_id"], name: "index_users_on_place_id"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
