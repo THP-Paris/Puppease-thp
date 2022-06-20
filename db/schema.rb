78d8f8082e54838382e9ffa1b5d67c73d15ff5ef
Ptitbab <brian.variengien@gmail.com>
2022-06-20 08:59:47 +0200

    div map, render form address in profile

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_16_124904) do
+ActiveRecord::Schema.define(version: 2022_06_17_140714) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -87,11 +87,13 @@ ActiveRecord::Schema.define(version: 2022_06_16_124904) do
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.string "slug"
-    t.boolean "admin", default: false
+    t.bigint "place_id"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["place_id"], name: "index_users_on_place_id"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["slug"], name: "index_users_on_slug", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "users", "places"
 end
