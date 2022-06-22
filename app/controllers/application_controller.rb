c37c67fc1e1f1e6aff8337d5b471eb5bfc695b54
Eva Laurent <eva@macbook-pro-de-eva.home>
2022-06-23 00:03:12 +0200

    wishlist assigned to current user when sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,12 @@
 class ApplicationController < ActionController::Base
+    after_action :create_wishlist_and_assign_to_current_user, only: [:create]
     before_action :configure_permitted_parameters, if: :devise_controller?
+
+    def create_wishlist_and_assign_to_current_user
+        if current_user != nil && current_user.wishlist == nil
+            wishlist =  Wishlist.create(user_id: current_user.id)
+        end
+    end
 
     protected
 def configure_permitted_parameters
