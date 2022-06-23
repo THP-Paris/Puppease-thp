d85f917e48a9940717a09a4360a6d49817d8642c
Eva Laurent <eva@macbook-pro-de-eva.home>
2022-06-23 12:17:48 +0200

    delete useless method in wishlist helper

--- app/helpers/wishlists_helper.rb
+++ app/helpers/wishlists_helper.rb
@@ -1,12 +1,6 @@
 module WishlistsHelper
     private
 
-    def require_login
-        unless current_user != nil
-            redirect_to new_user_session_path
-        end
-    end
-
     def only_current_wishlist
         if current_user != nil && current_user.wishlist.id != params[:id].to_i
             return redirect_to wishlist_path(current_user.wishlist.id)
